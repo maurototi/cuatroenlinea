3cfb0c745f67690176a7f6cdbecb2e6f9e490775
Mauro Nowell <102201400+maurototi@users.noreply.github.com>
2022-08-23 10:49:20 -0300

    Update pieza.php

--- app/pieza.php
+++ app/pieza.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace App;
+
+
+interface interfazFicha{
+
+    public function queColorSoy(); 
+}
+
+
+class Ficha implements interfazFicha
+{
+    protected string $color;
+
+
+
+    public function __construct ($colorIngresado) {
+
+        if($colorIngresado != "azul" && $colorIngresado != "rojo" ){
+            throw new Exception("La ficha tiene que ser de color rojo o azul");
+        }
+
+        $this->color = $colorIngresado;
+
+        
+    }
+
+    public function queColorSoy(){
+        return $this->color;
+    }
+
+}
